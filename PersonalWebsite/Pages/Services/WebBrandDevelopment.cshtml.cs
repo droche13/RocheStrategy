b200b5a82da306d44740c9b82c233be0c406c000
droche13 <devinroche13@gmail.com>
2023-04-17 19:48:03 -0400

    First commit

---
+++ PersonalWebsite/Pages/Services/WebBrandDevelopment.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace PersonalWebsite.Pages.Services
+{
+	public class WebBrandDevelopmentModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+        }
+
+       
+    }
+}
